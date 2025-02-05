deade4cf48146ab1a9740796f1f09d64335ecb18
atta541 <attareh542@gmail.com>
2025-02-05 19:57:11 +0500

    lastest

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,18 +1,10 @@
-
-
 from django.urls import path
-# from .views import (
-#      register_page
-# )
-
 from .auth.auth import (login_page,logout_view,register_page)
-
 from .chatbots.llama3 import Llama3APIView
 from .chatbots.mixtral import MixtralAPIView
 from .chatbots.gemma import GemmaAPIView
 from .chatbots.pdf import PdfAPIView, PdfChat
 from .chatbots.pdf import PdfAPIView
-
 from .chatbots.llama3_1 import Llama3_1APIView
 from .chatbots.atta import AttaAPIView
 from .chatbots.claude import ClaudeAPIView
@@ -46,95 +38,50 @@ from .visionmodels.llama32 import Llama32_11bVisionAPIView
 urlpatterns = [
 
     path('google-login/', google_login, name='google_login'),
-
     # path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/', login_page, name='login_page'),  
     path('register/', register_page, name='register'),
     path('forgot-password/', forgot_password, name='forgot-password'),
     path('reset-password/', reset_password, name='forgot-password'),
-
-
-
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('check_auth/', check_auth, name='check_auth'),
-
     path('api/logout/', logout_view, name='logout'),
     path('llama3/', Llama3APIView.as_view(), name='llama3'),
     path('llama3.1/', Llama3_1APIView.as_view(), name='llama3.1'),
     path('gpt3.5/', Gpt3_5APIView.as_view(), name='gpt-3.5'),
     path('islamicbot/', IslamicBotAPIView.as_view(), name='islamicbot'),
     path('gpt-4o/', Gpt4APIView.as_view(), name='gpt-4o'),
-
-
     path('cricketbot/', CricketbotAPIView.as_view(), name='cricketbot'),
-
-
     path('mixtral/', MixtralAPIView.as_view(), name='mixtral'),
     # path('gemini/', GeminiAPIView.as_view(), name='gemini'),
-
     path('LlamaVisionFree/', LlamaVisionFreeAPIView.as_view(), name='Llama-Vision'),
     # path('deepseek/', DeepSeekAPIView.as_view(), name='deepseek'),
-
     path('deepseek/', DeepSeekAPIView.as_view(), name='deepseek-api'),
     path('generate-image/', ImageGenerationAPIView.as_view(), name='generate-image-api'),
     path('tavilysearch/', TavilySearchAPIView.as_view(), name='tavilysearch-api'),
-
-
-
-
     # path('api/claude/', ClaudeAPIView.as_view(), name='claude'),
      path('claude/', ClaudeAPIView.as_view(), name='claude'),
     #  path('personalbot/', PersonalBotAPIView.as_view(), name='claude'),
-
-
-
-
     path('gemma/', GemmaAPIView.as_view(), name='gemma'),
-
     path('uol/', UolAPIView.as_view(), name='uol'),
     path('uolturbo/', UolturboAPIView.as_view(), name='uolturbo'),
-
-
     path('atta/', AttaAPIView.as_view(), name='atta'),
-
-
-    
     path('upload_pdf/', PdfAPIView.as_view(), name='upload_pdf'),
     path('pdfchat/', PdfChat.as_view(), name='pdfchat'),
-
     # //// csv chat
-
     path('upload_csv/', CsvAPIView.as_view(), name='upload_csv'),
     path('csvchat/', CsvChat.as_view(), name='csvchat'),
-
     # vision models 
-
     path('llama32.11b/', Llama32_11bVisionAPIView.as_view(), name='meta-llama/Llama-3.2-11B-Vision-Instruct-Turbo'),
-
     # end of vision models
-
-
-
-
     path('personalbot/', PersonalBotAPIView.as_view(), name='PersnalBotAPIView'),
-
     path('api/history_atta/<int:user_id>/', get_user_conversations, name='get_user_conversations'),
-    
     path('api/stripe/webhook', stripe_webhook, name='stripe_webhook'),
     # path('api/create-checkout-session/', create_checkout_session_view, name='create_checkout_session'),
     # path('checkout/', checkout, name='checkout'),
-
-
     path('api/admin/website-info/', website_info, name='website_info'),
     path('api/admin/all-data/', all_data, name='all_data'),
-
-
-
-
-
-
-
-
+# payment work 100%
     path('create-subscription/', create_subscription, name='create_subscription'),
 
 ]
